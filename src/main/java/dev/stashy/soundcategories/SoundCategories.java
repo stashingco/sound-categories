ae013edbf1198ffede07982f8c897903ec21a5ba
stashymane <aestheticrice@gmail.com>
2021-12-15 13:14:25 +0200

    Add category registration

--- src/main/java/dev/stashy/soundcategories/SoundCategories.java
+++ src/main/java/dev/stashy/soundcategories/SoundCategories.java
@@ -3,15 +3,30 @@ package dev.stashy.soundcategories;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.sound.SoundCategory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SoundCategories implements ClientModInitializer
 {
-    private List<SoundCategory> categories = new ArrayList<>();
+    private static Map<String, RegisterCallback> categories = new HashMap<>();
 
     @Override
     public void onInitializeClient()
     {
+    }
+
+    public static void registerCategory(String name, RegisterCallback callback)
+    {
+        categories.put(name, callback);
+    }
+
+    public static Map<String, RegisterCallback> getCategories()
+    {
+        return categories;
+    }
+
+    public interface RegisterCallback
+    {
+        void apply(SoundCategory cat);
     }
 }
