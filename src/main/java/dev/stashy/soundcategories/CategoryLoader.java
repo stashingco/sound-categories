884d2c94f7a2a253e22b4363b25eaed2c425aa10
stashymane <aestheticrice@gmail.com>
2021-12-18 16:26:12 +0200

    Add javadoc for annotations

--- src/main/java/dev/stashy/soundcategories/CategoryLoader.java
+++ src/main/java/dev/stashy/soundcategories/CategoryLoader.java
@@ -7,10 +7,16 @@ import java.lang.annotation.Target;
 
 public interface CategoryLoader
 {
+    /**
+     * Registers a SoundCategory and injects its reference to the field that has this annotation.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.FIELD)
     @interface Register
     {
+        /**
+         * The ID of the sound category - if omitted, will be automatically set from the field name.
+         */
         String id() default "";
     }
 }
