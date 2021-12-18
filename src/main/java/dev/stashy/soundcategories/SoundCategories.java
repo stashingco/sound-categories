e4452e6290c0a2422720af991b14fc80ea81bdb3
stashymane <aestheticrice@gmail.com>
2021-12-18 17:18:09 +0200

    Simplify getCallbacks

--- src/main/java/dev/stashy/soundcategories/SoundCategories.java
+++ src/main/java/dev/stashy/soundcategories/SoundCategories.java
@@ -5,10 +5,12 @@ import net.minecraft.sound.SoundCategory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class SoundCategories
 {
@@ -19,26 +21,26 @@ public class SoundCategories
         Map<String, RegisterCallback> categories = new HashMap<>();
         FabricLoader.getInstance().getEntrypoints("sound-categories", CategoryLoader.class)
                     .forEach(entry -> {
-                        Arrays.stream(entry.getClass().getDeclaredFields())
-                              .filter((f) -> f.isAnnotationPresent(CategoryLoader.Register.class))
-                              .forEach((it) -> {
-                                  String id = Objects.equals(it.getAnnotation(CategoryLoader.Register.class).id(),
-                                                             "")
-                                          ? it.getName() : it.getAnnotation(CategoryLoader.Register.class).id();
-                                  categories.put(id, cat -> {
-                                      try
-                                      {
-                                          it.set(entry, cat);
-                                      }
-                                      catch (IllegalAccessException e)
-                                      {
-                                          LOGGER.error("Unable to register sound category with ID {}", id);
-                                          e.printStackTrace();
-                                      }
-                                  });
-                              });
+                        getRegistrations(entry).forEach((it) -> {
+                            String id = Objects.equals(it.getAnnotation(CategoryLoader.Register.class).id(), "")
+                                    ? it.getName() : it.getAnnotation(CategoryLoader.Register.class).id();
+                            categories.put(id, cat -> {
+                                try {it.set(entry, cat);}
+                                catch (IllegalAccessException e)
+                                {
+                                    LOGGER.error("Unable to register sound category with ID {}", id);
+                                    e.printStackTrace();
+                                }
+                            });
+                        });
                     });
         return categories;
+    }
+
+    private static Stream<Field> getRegistrations(CategoryLoader loader)
+    {
+        return Arrays.stream(loader.getClass().getDeclaredFields())
+                     .filter(it -> it.isAnnotationPresent(CategoryLoader.Register.class));
     }
 
     public interface RegisterCallback
