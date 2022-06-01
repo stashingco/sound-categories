66ad112aa9de878c9b0ee886e954a0855d07a2b3
stashymane <aestheticrice@gmail.com>
2022-06-01 23:05:05 +0300

    Fix OptiFine default volume (properly)

--- src/main/java/dev/stashy/soundcategories/mixin/GameOptionsMixin.java
+++ src/main/java/dev/stashy/soundcategories/mixin/GameOptionsMixin.java
@@ -1,26 +1,30 @@
 package dev.stashy.soundcategories.mixin;
 
 import dev.stashy.soundcategories.SoundCategories;
+import it.unimi.dsi.fastutil.objects.Object2FloatMap;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.sound.SoundCategory;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(GameOptions.class)
-public class GameOptionsMixin
+public abstract class GameOptionsMixin
 {
-    @Inject(at = @At(value = "HEAD"), method = "method_33667", cancellable = true, require = 0)
-    private static void getDefaultFloat(GameOptions.Visitor visitor, SoundCategory category, Float currentLevel, CallbackInfoReturnable<Float> cir)
+    @Shadow
+    @Final
+    private Object2FloatMap<SoundCategory> soundVolumeLevels;
+
+    @Inject(at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;computeFloat(Ljava/lang/Object;Ljava/util/function/BiFunction;)F"), method = "accept", locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void getDefaultFloat(GameOptions.Visitor visitor, CallbackInfo ci, SoundCategory var2[], int var3, int var4, SoundCategory keyBinding)
     {
-        cir.setReturnValue(
-                visitor.visitFloat(
-                        "soundCategory_" + category.getName(),
-                        currentLevel != null
-                                ? currentLevel
-                                : SoundCategories.defaultLevels.getOrDefault(category, 1.0F)
-                )
-        );
+        SoundCategories.defaultLevels.keySet().forEach(it -> {
+            soundVolumeLevels.put(it, visitor.visitFloat("soundCategory_" + it.getName(),
+                                                         SoundCategories.defaultLevels.get(it)));
+        });
     }
 }
